7539efac5021541faa278a9064c45d51f91e1810
pavlo.zaloznyi <pavlo.zaloznyi@pm.bet>
2022-08-27 23:03:44 +0300

    the first part

---
+++ src/main/java/com/pzaloznyi/cursorspringboot/GlobalExceptionHandler.java
@@ -0,0 +1,14 @@
+package com.pzaloznyi.cursorspringboot;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleException() {
+        return ResponseEntity.badRequest().body("Wrong argument.");
+    }
+}
